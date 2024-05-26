ba550c61c8c39a1affc74bc53f40907191dde64e
eduardojpsena <eduardojpsena@gmail.com>
2024-05-26 11:20:26 -0300

    feat: initial config commit

---
+++ src/main/java/com/api_financeira_simplificada/ApiFinanceiraSimplificadaApplication.java
@@ -0,0 +1,13 @@
+package com.api_financeira_simplificada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFinanceiraSimplificadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFinanceiraSimplificadaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api_financeira_simplificada/ApiFinanceiraSimplificadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api_financeira_simplificada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFinanceiraSimplificadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
