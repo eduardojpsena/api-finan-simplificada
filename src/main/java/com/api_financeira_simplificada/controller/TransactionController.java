9481edad565e08f6ec2c38a59880b2446db9bf42
eduardojpsena <eduardojpsena@gmail.com>
2024-05-27 17:31:46 -0300

    refactor: adding @RequestBody on controllers

--- src/main/java/com/api_financeira_simplificada/controller/TransactionController.java
+++ src/main/java/com/api_financeira_simplificada/controller/TransactionController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +22,7 @@ public class TransactionController {
     private TransactionService transactionService;
 
     @PostMapping()
-    public ResponseEntity<Transaction> createTransaction(TransactionDTO request) throws Exception {
+    public ResponseEntity<Transaction> createTransaction(@RequestBody TransactionDTO request) throws Exception {
         Transaction response = transactionService.createTransaction(request);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
--- src/main/java/com/api_financeira_simplificada/controller/UserController.java
+++ src/main/java/com/api_financeira_simplificada/controller/UserController.java
@@ -3,14 +3,12 @@ package com.api_financeira_simplificada.controller;
 import com.api_financeira_simplificada.domain.user.User;
 import com.api_financeira_simplificada.dto.UserDTO;
 import com.api_financeira_simplificada.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,13 +21,14 @@ public class UserController {
     private UserService userService;
 
     @GetMapping(value = "/list")
+    @Operation()
     public ResponseEntity<List<User>> listUsers() {
         List<User> users = userService.findAllUsers();
         return new ResponseEntity<>(users, HttpStatus.OK);
     }
 
     @PostMapping()
-    public ResponseEntity<User> createUser(UserDTO request) {
+    public ResponseEntity<User> createUser(@RequestBody UserDTO request) {
         User response = userService.createUser(request);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
